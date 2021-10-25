c5ee913354a72cec74d9f74900adecf046e46cfb
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-10-24 22:13:01 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI)
 
 const app = express();
 
 require('./routes/index')(app);
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(5000);
